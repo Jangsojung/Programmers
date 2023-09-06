bf31ae0f466b894bd6401db3573477b7dae28e3c
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2023-09-06 16:25:13 +0900

    9일차 commit

---
+++ src/Level1/_176963_MemoryScore.java
@@ -0,0 +1,25 @@
+package Level1;
+
+import java.util.HashMap;
+
+public class _176963_MemoryScore {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < name.length ; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for(int i = 0 ; i < photo.length ; i++) {
+            for(int j = 0 ; j < photo[i].length ; j++) {
+                if(map.containsKey(photo[i][j])) {
+                    answer[i] += map.get(photo[i][j]);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
