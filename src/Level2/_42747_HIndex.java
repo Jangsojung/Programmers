eb23e477599bd630377c437c7529d183c20e7fe8
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2023-10-12 12:31:43 +0900

    23일차 commit

---
+++ src/Level2/_42747_HIndex.java
@@ -0,0 +1,22 @@
+package Level2;
+
+import java.util.Arrays;
+
+public class _42747_HIndex {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for(int i = 0 ; i < citations.length ; i++) {
+            int h = citations.length - i;
+
+            if(citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
