17d5c9535893016119365b280ae3b6caaae3ebd3
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-03-25 17:54:22 +0900

    feat: Programmers_Training_strPrint

---
+++ src/Training/Day1/_181952_strPrint.java
@@ -0,0 +1,11 @@
+package Training.Day1;
+
+import java.io.*;
+
+public class _181952_strPrint {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine());
+    }
+}
