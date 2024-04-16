0459d5d44fe71a3a5f3c6dad3b60181c87ea10c2
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-04-16 16:39:48 +0900

    feat: Programmers_Lv2_BiggestNumber

---
+++ src/Level2/_42746_BiggestNumber.java
@@ -0,0 +1,26 @@
+package Level2;
+
+import java.util.Arrays;
+
+public class _42746_BiggestNumber {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
