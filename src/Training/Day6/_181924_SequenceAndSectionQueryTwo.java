5774d99dda624f70b589237c75d8e03cafd60a26
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-04-25 13:49:13 +0900

    feat: Programmers_Training_SequenceAndSectionQueryTwo

---
+++ src/Training/Day6/_181924_SequenceAndSectionQueryTwo.java
@@ -0,0 +1,19 @@
+package Training.Day6;
+
+public class _181924_SequenceAndSectionQueryTwo {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (arr[j] > queries[i][2]) {
+                    min = Math.min(arr[j], min);
+                }
+            }
+            answer[i] = min == Integer.MAX_VALUE ? -1 : min;
+        }
+
+        return answer;
+    }
+}
