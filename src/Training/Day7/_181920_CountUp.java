e20999e74ac19d8ac5c1c732fc0aa9ca4a182633
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-03 13:36:22 +0900

    feat: Programmers_Training_CountUp

---
+++ src/Training/Day7/_181920_CountUp.java
@@ -0,0 +1,14 @@
+package Training.Day7;
+
+public class _181920_CountUp {
+    public int[] solution(int start_num, int end_num) {
+        int size = end_num - start_num + 1;
+        int[] answer = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            answer[i] = i + start_num;
+        }
+
+        return answer;
+    }
+}
