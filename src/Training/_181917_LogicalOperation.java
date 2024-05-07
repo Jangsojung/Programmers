f767070c89a440581d79bb61d0857fd00c5e81c0
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-07 21:03:50 +0900

    feat: Programmers_Training_LogicalOperation

---
+++ src/Training/_181917_LogicalOperation.java
@@ -0,0 +1,7 @@
+package Training;
+
+public class _181917_LogicalOperation {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        return (x1 || x2) && (x3 || x4);
+    }
+}
