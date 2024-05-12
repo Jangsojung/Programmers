e683ccfd5fe961f5d7fb0f4e61fa2675bd166dcc
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-12 21:05:48 +0900

    feat: Programmers_Training_BehindStringN

---
+++ src/Training/Day9/_181910_BehindStringN.java
@@ -0,0 +1,7 @@
+package Training.Day9;
+
+public class _181910_BehindStringN {
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length() - n);
+    }
+}
