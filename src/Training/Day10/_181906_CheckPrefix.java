4876c5f2865e229b8f0c00371985d7a3a1fc688b
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-14 15:53:47 +0900

    feat: Programmers_Training_CheckPrefix

---
+++ src/Training/Day10/_181906_CheckPrefix.java
@@ -0,0 +1,7 @@
+package Training.Day10;
+
+public class _181906_CheckPrefix {
+    public int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
