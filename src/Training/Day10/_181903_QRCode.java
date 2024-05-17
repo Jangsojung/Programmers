b19a1e4ee37adb544fe429cb92c49c0baaea0010
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-17 13:11:24 +0900

    feat: Programmers_Training_QRCode

---
+++ src/Training/Day10/_181903_QRCode.java
@@ -0,0 +1,15 @@
+package Training.Day10;
+
+public class _181903_QRCode {
+    public String solution(int q, int r, String code) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) {
+                answer.append(code.charAt(i));
+            }
+        }
+
+        return answer.toString();
+    }
+}
