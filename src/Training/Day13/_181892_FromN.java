b400b697b188a94ad8e5e19812c01ccfeb6a2956
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-28 19:04:56 +0900

    feat: Programmers_Training_FromN

---
+++ src/Training/Day13/_181892_FromN.java
@@ -0,0 +1,21 @@
+package Training.Day13;
+
+import java.util.ArrayList;
+
+public class _181892_FromN {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = n - 1; i < num_list.length; i++) {
+            list.add(num_list[i]);
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
