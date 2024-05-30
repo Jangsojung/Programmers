404982267649f0a3cbc86bd58dda7ac31de8d13e
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-05-30 16:30:43 +0900

    feat: Programmers_Training_ToNElement

---
+++ src/Training/Day13/_181889_ToNElement.java
@@ -0,0 +1,9 @@
+package Training.Day13;
+
+import java.util.Arrays;
+
+public class _181889_ToNElement {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
