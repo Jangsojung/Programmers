bdae6bc7d8915534b235ea08d40cde2840a7747b
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-06-20 21:31:06 +0900

    feat: Programmers_Training__181882_CalculationsBasedOnLength

---
+++ src/Training/Day15/_181879_CalculationsBasedOnLength.java
@@ -0,0 +1,21 @@
+package Training.Day15;
+
+public class _181879_CalculationsBasedOnLength {
+    public int solution(int[] num_list) {
+        int answer = 0;
+
+        if (num_list.length >= 11) {
+            for (int i : num_list) {
+                answer += i;
+            }
+        } else {
+            answer = 1;
+
+            for (int i : num_list) {
+                answer *= i;
+            }
+        }
+
+        return answer;
+    }
+}
