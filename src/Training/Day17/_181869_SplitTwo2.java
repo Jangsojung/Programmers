37d24efe99ba21192527b01009293ffe6e08db67
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2024-07-12 15:34:58 +0900

    feat: Programmers_Training_SplitTwo2

---
+++ src/Training/Day17/_181869_SplitTwo2.java
@@ -0,0 +1,9 @@
+package Training.Day17;
+
+import java.util.ArrayList;
+
+public class _181869_SplitTwo2 {
+    public String[] solution(String my_string) {
+        return my_string.trim().split("\\s+");
+    }
+}
